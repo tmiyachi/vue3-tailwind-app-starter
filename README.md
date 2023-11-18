2ad4e7633afea823e9171b593fa17f831274ce0b
tmiyachi <t.miyachi@hotmail.co.jp>
2023-11-18 14:07:36 +0900

    Viteへ移行

--- README.md
+++ README.md
@@ -4,15 +4,10 @@ Vue.js を用いた web アプリ開発のための個人用スターター
 
 ## Description
 
-- webpack によるバンドル
+- Vite によるバンドル
 - ESlint，stylelint による静的コード解析
 - Prettier によるフォーマット
-- Babel によるトランスコンパイル
 - tailwindcss の利用
-
-## Note
-
-[eslint-plugin-vue](https://github.com/vuejs/eslint-plugin-vue) は最新バージョンを使用しVue3準拠としている．
 
 ## Usage
 
---
+++ vite.config.js
@@ -0,0 +1,79 @@
+/* eslint-env node */
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+const license = require('rollup-plugin-license');
+
+const BANNER = `
+/*! FOLLOWING LIBRARIES ARE USED.
+ * - vue (https://github.com/vuejs/core/blob/main/LICENSE)
+ *   The MIT License (MIT)
+ *   Copyright (c) 2018-present, Yuxi (Evan) You
+ *
+ * - tailwindcss (https://github.com/tailwindlabs/tailwindcss/blob/master/LICENSE)
+ *   The MIT License (MIT)
+ *   Copyright (c) Tailwind Labs, Inc.
+ */`;
+
+export default defineConfig({
+  plugins: [vue()],
+
+  root: path.resolve(__dirname, 'src'),
+  publicDir: path.resolve(__dirname, 'public'),
+  resolve: {
+    extensions: ['.vue', '.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+
+  build: {
+    // target: ['firefox86'], // バンドルのブラウザ互換性のターゲットを指定する
+    outDir: '../dist',
+    emptyOutDir: true,
+    assetsDir: 'assets',
+    // assetsInlineLimit: 0,  // アセットのをbase64インライン化を無効にする場合
+    // sourcemap: true,
+
+    // assetsInclude
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].bundle.js',
+        chunkFileNames: `js/[name].js`, // vendor chunk filenames
+        assetFileNames: `assets/[name].[ext]`, // CSSもassetsになる
+      },
+      plugins: [
+        license({
+          sourcemap: true,
+          cwd: process.cwd(), // The default
+
+          banner: BANNER,
+          thirdParty: {
+            // includePrivate: true, // Default is false.
+            // multipleVersions: true, // Default is false.
+            // output: {
+            //   file: path.join(__dirname, 'dist', 'DEPENDENCIES.txt'),
+            // },
+          },
+        }),
+      ],
+    },
+  },
+
+  define: {
+    __VUE_OPTIONS_API__: false, // Option API を使う場合はtrue
+    __VUE_PROD_DEVTOOLS__: false,
+  },
+
+  server: {
+    port: 8080,
+    open: true,
+    proxy: {
+      // '/api': 'http://localhost:8081',
+      // '/api': {
+      //  target: 'http://example.com',
+      //  changeOrigin: true,
+      //  rewrite: (path) => path.replace(/^\/api/, ''),
+    },
+  },
+});
